b131388100eb11f2cb6f1ab66894bdee84941568
iamconnaught <wdonnelly22@gmail.com>
2019-06-03 20:54:50 -0500

    Created Countdown component for user profile

--- src/User/index.js
+++ src/User/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link }from 'react-router-dom';
-import RandomName from '../RandomName'
+import RandomName from '../RandomName';
+import Countdown from '../Countdown';
 
 class User extends Component {
 	render(){
@@ -8,6 +9,7 @@ class User extends Component {
 			<div>
 				<h1>User Profile</h1>
 				<RandomName />
+				<Countdown timeTilDate="06 26 2019, 6:00 am" timeFormat="MM DD YYYY, h:mm a"/>
 			</div>
 
 			)
