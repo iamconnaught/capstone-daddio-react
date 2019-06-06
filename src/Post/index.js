da7ac133970571c2b9034af13a7de2de88867b24
iamconnaught <wdonnelly22@gmail.com>
2019-06-06 17:10:33 -0500

    conditionally render the blog

--- src/Post/index.js
+++ src/Post/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import EditPost from '../EditPost';
+import ShowPost from '../ShowPost';
 
 class Post extends Component {
 	constructor(){
 		super();
 		this.state ={
 			list: [],
-			idOfPostBeingEdited: null
+			idOfPostBeingShown: null
+			// idOfPostBeingEdited: null
 		}
 	}
 	async componentDidMount(){
@@ -22,55 +23,51 @@ class Post extends Component {
 			list: fetchedData
 		})
 	}
-	deletePost = async (e) => {
-		e.preventDefault();
-		console.log('delete hit');
-		console.log(e.currentTarget.parentNode.dataset);
-		try {
-			const deletedPost = await fetch(`${process.env.REACT_APP_BACKEND_URL}/post/${e.currentTarget.parentNode.dataset.postId}`, {
-				method: 'DELETE',
-				credentials: 'include'
-			})
-			console.log(deletedPost);
-			this.componentDidMount();
-		} catch (err){
-			console.error(err)
-		}
-	}
-	closeEdit = () => {
-		this.getPostList();
+	// deletePost = async (e) => {
+	// 	e.preventDefault();
+	// 	console.log('delete hit');
+	// 	console.log(e.currentTarget.parentNode.dataset);
+	// 	try {
+	// 		const deletedPost = await fetch(`${process.env.REACT_APP_BACKEND_URL}/post/${e.currentTarget.parentNode.dataset.postId}`, {
+	// 			method: 'DELETE',
+	// 			credentials: 'include'
+	// 		})
+	// 		console.log(deletedPost);
+	// 		this.componentDidMount();
+	// 	} catch (err){
+	// 		console.error(err)
+	// 	}
+	// }
+	// closeEdit = () => {
+	// 	this.getPostList();
+	// 	this.setState({
+	// 		idOfPostBeingEdited: null
+	// 	})
+	// }
+	// editPost = async (e) => {
+	// 	console.log(e.currentTarget.parentNode.dataset);
+	// 	const id = e.currentTarget.parentNode.dataset.postId
+	// 	console.log("id");
+	// 	console.log(id);
+	// 	this.setState({
+	// 		idOfPostBeingEdited: id
+	// 	})
+	// }
+	showPost = (e) => {
+		const id = e.currentTarget.parentNode.dataset.postId;
 		this.setState({
-			idOfPostBeingEdited: null
-		})
-	}
-	editPost = async (e) => {
-		console.log(e.currentTarget.parentNode.dataset);
-		const id = e.currentTarget.parentNode.dataset.postId
-		console.log("id");
-		console.log(id);
-		this.setState({
-			idOfPostBeingEdited: id
+			idOfPostBeingShown: id
 		})
 	}
 
 	render(){
 		const postList = this.state.list.map((post) => {
-			const keywords = post.keywords.map((keyword, i) => {
-				return(
-					<li key={i}>
-					{keyword}
-					</li>
-				)
-			})
 			return(
 				<div>
 					<li data-post-id={post._id} key={post._id}>
 						<span>{post.title}</span><br/>
-						<span>{post.text}</span><br/>
-						{this.state.idOfPostBeingEdited !== null ? <EditPost idOfPostBeingEdited={this.state.idOfPostBeingEdited} closeEdit={this.closeEdit}/> : null}
-						<ul>{keywords}</ul>
-						<button onClick={this.deletePost}>Delete</button>
-						<button onClick={this.editPost}>Edit</button>
+						
+						{this.state.idOfPostBeingShown === null ? <button onClick={this.showPost}>Show</button> : null}
 					</li>
 				</div>
 			)
@@ -78,7 +75,8 @@ class Post extends Component {
 		return(
 			<div>
 				<h1>Blog</h1>
-				<ul>{postList}</ul>
+				{this.state.idOfPostBeingShown === null ? <ul>{postList}</ul> : null }
+				{this.state.idOfPostBeingShown !== null ? <ShowPost idOfPostBeingShown={this.state.idOfPostBeingShown} /> : null}
 			</div>	
 		)
 	}
---
+++ src/ShowPost/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react';
+import EditPost from '../EditPost';
+
+class ShowPost extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			title: '',
+			text: '',
+			keywords: [],
+			idOfPostBeingEdited: null
+		}
+	}
+	componentDidMount(){
+		// console.log('this.props.idOfTaskBeingShown at componentDidMount');
+		// console.log(this.props.idOfTaskBeingShown);
+		this.getPost();
+	}
+	getPost = async () => {
+		// console.log('this.props.idOfTaskBeingShown at getTask');
+		// console.log(this.props.idOfTaskBeingShown);
+		const data = await fetch(`${process.env.REACT_APP_BACKEND_URL}/post/${this.props.idOfPostBeingShown}`, {
+			method: 'GET',
+			credentials: 'include'
+		});
+		const fetchedData = await data.json();
+		this.setState({
+			title: fetchedData.title,
+			text: fetchedData.text,
+			keywords: fetchedData.keywords
+		})
+	}
+	editPost = async (e) => {
+		// console.log(e.currentTarget.parentNode.dataset);
+		// const id = e.currentTarget.parentNode.dataset.taskId
+		// console.log('id');
+		// console.log(id);
+		this.setState({
+			idOfPostBeingEdited: this.props.idOfPostBeingShown
+		})
+	}
+	closeEdit = () => {
+		this.componentDidMount();
+		this.setState({
+			idOfPostBeingEdited: null
+		})
+	}
+	deletePost = async (e) => {
+		e.preventDefault();
+		try {
+			// console.log('delete this id');
+			// console.log(e.currentTarget.parentNode.dataset);
+			await fetch(`${process.env.REACT_APP_BACKEND_URL}/post/${this.props.idOfPostBeingShown}`, {
+				method: "DELETE",
+				credentials: 'include'
+			})
+			this.componentDidMount()
+		} catch (err){
+			console.error(err);
+		}
+	}
+	render(){
+		console.log('this.state.text in showpost render');
+		console.log(this.state.text);
+		const keywords = this.state.keywords.map((keyword, i) => {
+				return(
+					<li key={i}>
+					{keyword}
+					</li>
+				)
+			})
+		return(
+			<div>
+				<p>{this.state.text}</p>
+				<ul>{keywords}</ul>
+				{this.state.idOfPostBeingEdited !== null ? <EditPost idOfPostBeingEdited={this.state.idOfPostBeingEdited} closeEdit={this.closeEdit}/> : null}
+				<button onClick={this.deletePost}>Delete</button>
+				<button onClick={this.editPost}>Edit</button>
+			</div>
+			)
+
+	}
+}
+export default ShowPost;
