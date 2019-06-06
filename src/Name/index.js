8bcf85ece74468de6dc6a13a6a84d485426959f1
iamconnaught <wdonnelly22@gmail.com>
2019-06-05 20:45:48 -0500

    delete button working for names

--- src/Name/index.js
+++ src/Name/index.js
@@ -18,11 +18,27 @@ class Name extends Component {
 			list: fetchedData
 		});
 	}
+	deleteName = async (e) => {
+		e.preventDefault();
+		console.log(e.currentTarget.parentNode.dataset);
+		try {
+			const deletedName = await fetch(process.env.REACT_APP_BACKEND_URL + '/name/' + e.currentTarget.parentNode.dataset.nameId, {
+				method: "DELETE",
+				credentials: 'include'
+			})
+			console.log('deletedName');
+			console.log(deletedName);
+			this.componentDidMount()		
+		} catch (err){
+			console.error(err)
+		}
+	}
 	render() {
 		const nameList = this.state.list.map((name) => {
 			return(
 				<li data-name-id={name._id} key={name._id}>
 					<span>{name.name}</span>
+					<button onClick={this.deleteName}>Delete</button>
 				</li>
 
 				)
